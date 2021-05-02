9fe3b413d53e5270ff5489407f7f10916059e6a6
Melanie Fayne <61229069+mel-fayne@users.noreply.github.com>
2021-05-02 15:16:23 +0300

    implemented treatment tab functionality

--- lib/app/screens/medical_history_tabs/treatmenttab.dart
+++ lib/app/screens/medical_history_tabs/treatmenttab.dart
@@ -1,57 +1,175 @@
+import 'package:Tabibu/app/models/diagnosis.dart';
 import 'package:Tabibu/app/theme/colors.dart';
 import 'package:Tabibu/app/theme/my_custom_icons_icons.dart';
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 class TreatmentTab extends StatefulWidget {
   static const routeName = "/treatmenttab";
-
+//accepting parameters from previous screen
+  final String ptid;
+  TreatmentTab({@required this.ptid});
   @override
-  _TreatmentTabState createState() => _TreatmentTabState();
+  State<StatefulWidget> createState() {
+    return TreatmentTabState(this.ptid);
+  }
 }
 
-class _TreatmentTabState extends State<TreatmentTab> {
+class TreatmentTabState extends State<TreatmentTab> {
+  String ptid;
+  TreatmentTabState(this.ptid);
+
+  @override
+  void initState() {
+    super.initState();
+    getDiagnosisList();
+  }
+
+  List<Diagnosis> diagnosisdata = [];
+
+  getDiagnosisList() async {
+    var url = "http://192.168.0.15/tabibu/api/diagnosis/getdiagnosis.php";
+    var res = await http.post(url,
+        body: {"pt_id": ptid}, headers: {"Accept": "application/json"});
+    var diagnosis = json.decode(res.body);
+    if (diagnosis == "error") {
+      print('Unexpected error occured!');
+    } else {
+      for (var data in diagnosis) {
+        diagnosisdata.add(new Diagnosis(
+          data['disease'],
+          data['description'],
+          data['date'],
+          data['weight'],
+          data['temp'],
+          data['pulse'],
+          data['pressure'],
+          data['symptoms'],
+          data['medicine'],
+          data['prescription'],
+          data['treatmentinfo'],
+          data['recordid'],
+          data['dr_id'],
+          data['pt_id'],
+        ));
+      }
+      setState(() {});
+      diagnosisdata.forEach((someData) => print("Name : ${someData.recordid}"));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            padding: EdgeInsets.only(left: 10, top: 10),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: Text(
-                  'Treatments',
+        body: Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: diagnosisdata.length == 0
+          ? Center(
+              child: Text('Oops, No record yet!',
                   style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'PT Serif',
-                      fontWeight: FontWeight.w600,
-                      fontSize: 22),
-                ),
-              ),
-              Container(
-                  width: double.infinity,
-                  child: Column(children: [
-                    ListTile(
-                      leading: Icon(
-                        MyCustomIcons.pill,
-                        color: kPrimaryGreen,
-                        size: 55,
-                      ),
-                      title: Text('Diagnosis: ...\nMedicine: ...',
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontFamily: 'Source Sans',
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black)),
-                      subtitle: Text(
-                          "Date: ...\n Prescription: ...\n Additional Info: ...",
-                          style: TextStyle(
-                              fontSize: 14,
-                              fontFamily: 'Source Sans',
-                              fontWeight: FontWeight.w600,
-                              color: kFieldTextColor)),
-                    ),
-                  ])),
-            ])));
+                    fontFamily: 'PT Serif',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w700,
+                    color: kPrimaryGreen,
+                  )))
+          : Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+              child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: diagnosisdata.length,
+                  itemBuilder: (_, index) {
+                    return Card(
+                        color: Colors.blue[100],
+                        elevation: 7.0,
+                        child: ListTile(
+                          leading: Icon(
+                            MyCustomIcons.pill,
+                            color: kPrimaryGreen,
+                            size: 35,
+                          ),
+                          title: Padding(
+                              padding: EdgeInsets.only(top: 5),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  textProfile(
+                                    label: 'Ailment:',
+                                    text: '${diagnosisdata[index].disease}',
+                                  ),
+                                  textProfile(
+                                    label: 'Medicine:',
+                                    text: '${diagnosisdata[index].medicine}',
+                                  ),
+                                  textProfile(
+                                      label: 'Prescription:',
+                                      text:
+                                          '${diagnosisdata[index].prescription}'),
+                                ],
+                              )),
+                          subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Additional Treatment Information:',
+                                        style: TextStyle(
+                                          color: kFieldTextColor,
+                                          fontSize: 13,
+                                          fontFamily: 'PT Serif',
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                      Text(
+                                        '${diagnosisdata[index].treatmentinfo}',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 14,
+                                          fontFamily: 'Source Sans',
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      )
+                                    ]),
+                                textProfile(
+                                  label: 'Date:',
+                                  text: '${diagnosisdata[index].date}',
+                                ),
+                              ]),
+                        ));
+                  })),
+    ));
   }
+}
+
+Widget textProfile({label, text}) {
+  return Padding(
+    padding: EdgeInsets.symmetric(vertical: 2.0),
+    child: Row(
+      children: [
+        Text(label,
+            style: TextStyle(
+              color: kFieldTextColor,
+              fontSize: 13,
+              fontFamily: 'PT Serif',
+              fontWeight: FontWeight.w600,
+            )),
+        Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: Text(
+              text,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14,
+                fontFamily: 'Source Sans',
+                fontWeight: FontWeight.w600,
+              ),
+            ))
+      ],
+    ),
+  );
 }
